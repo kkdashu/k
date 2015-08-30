4230fa9bacad90e509b5a7a407d8af1122f61bdb
kkdashu <wmeng17@gmail.com>
2015-08-30 23:27:34 +0800

    使用base.js

--- app/scripts/ui/tab_menu/tab_menu.js
+++ app/scripts/ui/tab_menu/tab_menu.js
@@ -1,29 +1,22 @@
 require('./tab_menu.css');
-var template = require("./tab_menu.hbs");
+var template = require('./tab_menu.hbs');
+var Base = require('../../lib/base.js');
 
-function TabMenu(data) {
-  this.data = data;
-  this.$el = $("<div class='tab_menu'></div>");
-  this.tmp = template;
-}
-
-TabMenu.prototype.init = function() {
-
-}
-
-TabMenu.prototype.render = function(parentEl) {
-  parentEl = parentEl || $('body');
-  var html = this.tmp({model: this.data});
-  this.$el.html(html);
-  parentEl.append(this.$el);
-}
-
-TabMenu.prototype.hide = function() {
-  this.$el.hide();
-}
-
-TabMenu.prototype.show = function() {
-  this.$el.show();
-}
+var TabMenu = Base.extend({
+  $el: $("<div class='tab_menu'></div>"),
+  tmp: template,
+  render: function(parentEl) {
+    parentEl = parentEl || $('body');
+    var html = this.tmp({model: this.model});
+    this.$el.html(html);
+    parentEl.append(this.$el);
+  },
+  hide: function() {
+    this.$el.hide();
+  },
+  show: function() {
+    this.$el.show();
+  }
+})
 
 module.exports = TabMenu;
