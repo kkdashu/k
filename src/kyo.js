038a7807fa79d0767b32ab4996c42e2e3539ed93
wangmeng <wmeng17@gmail.com>
2015-09-01 13:31:59 +0800

    移除掉component.js

--- src/kyo.js
+++ src/kyo.js
@@ -1,14 +1,12 @@
 var Class = require('./class.js'),
     Events = require('./events.js'),
     Base = require('./base.js'),
-    Component = require('./component.js'),
     _ = require('./underscore.js');
 
 var K = {
   Class: Class,
   Events: Events,
   Base: Base,
-  Component: Component,
   _: _
 }
 
