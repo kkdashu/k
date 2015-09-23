cd7113c7742fd1eb2880ecbcb4f94b9b2fe55075
kkdashu <wmeng17@gmail.com>
2015-09-23 14:21:35 +0800

    增加注释

--- src/class.js
+++ src/class.js
@@ -1,12 +1,42 @@
 /**
  * 模拟javascript的oo实现
+ * base
+ * var instance = Class.create({
+ * 	name: 'kkdashu',
+ * 	age: 28
+ * });
+ * console.assert(instance.name, 'kkdashu');
+ * console.assert(instance.age,  28);
+ *
+ *
  * var Person = Class.extend({
  *   say: function() {
  *    console.log('My name is ' + this.name);
  *   }
  * });
  *
- * var kkdashu = new Person.create({name: 'kkdashu'});
+ * var kkdashu = Person.create({
+ * 		name: 'kkdashu'
+ * 		show: function() {
+ * 	 		console.log(this.name);
+ * 	  }
+ * 	});
+ *
+ * //通过extend实现继承效果
+ * //extend会把属性或者方法附加到构造函数的原型上
+ * //create会把属性附加到实例对象上（会覆盖掉原型上的属性）
+ * var Student = Person.extend({
+ * 	grade: '2'
+ * });
+ * var s = Student.create({
+ * 	name: 'wmeng',
+ * 	age: 28,
+ * 	grade: 11
+ * });
+ *
+ * console.assert(s.name, 'wmeng');
+ * console.assert(s.age, 28);
+ * console.assert(s.grade, 11);
  *
  *
  **/
@@ -22,6 +52,7 @@ Class.extend = function() {
 
   //返回的构造函数
   function SubClass() {
+    //获取调用create方法时传递过来的参数
     var properties = _.toArray(arguments);
     while(property = properties.shift()) {
       _.extend(this, property);
@@ -31,9 +62,9 @@ Class.extend = function() {
     }
   }
   //继承当前类
-  SubClass.prototype = new this();
+  SubClass.prototype = new Constructor();
 
-  //把所有传过来的属性与方法赋值给原型
+  //调用extend时把所有传过来的属性与方法赋值给原型
   var properties = _.toArray(arguments);
   while(property = properties.shift()) {
     _.extend(SubClass.prototype, property);
@@ -55,6 +86,7 @@ Class.create = function() {
       }
     }
   }
+  // this可能是Ｃlass或者SubClass
   if(this == Class){
     return new _Class(arg);
   }
